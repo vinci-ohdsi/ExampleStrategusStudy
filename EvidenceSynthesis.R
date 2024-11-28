baf455d9c10aae4b0cc59c373d8dc49e3b89bef9
Admin_mschuemi <Admin_mschuemi@its.jnj.com>
2024-11-28 07:43:07 -0500

    Adding evidence synth script and upload results from zip files

---
+++ EvidenceSynthesis.R
@@ -0,0 +1,82 @@
+################################################################################
+# INSTRUCTIONS: The code below assumes you uploaded results to a PostgreSQL 
+# database per the UploadResults.R script.This script will create the 
+# analysis specification for running the EvidenceSynthesis module, execute
+# EvidenceSynthesis, create the results tables and upload the results.
+# 
+# Review the code below and note the "sourceMethod" parameter used in the
+# esModuleSettingsCreator$createEvidenceSynthesisSource() function. If your 
+# study is not using CohortMethod and/or SelfControlledCaseSeries you should
+# remove that from the evidenceSynthesisAnalysisList.
+# ##############################################################################
+
+library(dplyr)
+library(Strategus)
+
+resultsDatabaseConnectionDetails <- DatabaseConnector::createConnectionDetails(
+  dbms = "sqlite",
+  server = "E:/exampleStrategusStudy/Results.sqlite"
+)
+resultsDatabaseSchema <- "main"
+
+esModuleSettingsCreator = EvidenceSynthesisModule$new()
+evidenceSynthesisSourceCm <- esModuleSettingsCreator$createEvidenceSynthesisSource(
+  sourceMethod = "CohortMethod",
+  likelihoodApproximation = "adaptive grid"
+)
+metaAnalysisCm <- esModuleSettingsCreator$createBayesianMetaAnalysis(
+  evidenceSynthesisAnalysisId = 1,
+  alpha = 0.05,
+  evidenceSynthesisDescription = "Bayesian random-effects alpha 0.05 - adaptive grid",
+  evidenceSynthesisSource = evidenceSynthesisSourceCm
+)
+evidenceSynthesisSourceSccs <- esModuleSettingsCreator$createEvidenceSynthesisSource(
+  sourceMethod = "SelfControlledCaseSeries",
+  likelihoodApproximation = "adaptive grid"
+)
+metaAnalysisSccs <- esModuleSettingsCreator$createBayesianMetaAnalysis(
+  evidenceSynthesisAnalysisId = 2,
+  alpha = 0.05,
+  evidenceSynthesisDescription = "Bayesian random-effects alpha 0.05 - adaptive grid",
+  evidenceSynthesisSource = evidenceSynthesisSourceSccs
+)
+evidenceSynthesisAnalysisList <- list(metaAnalysisCm, metaAnalysisSccs)
+evidenceSynthesisAnalysisSpecifications <- esModuleSettingsCreator$createModuleSpecifications(
+  evidenceSynthesisAnalysisList
+)
+esAnalysisSpecifications <- Strategus::createEmptyAnalysisSpecificiations() |>
+  Strategus::addModuleSpecifications(evidenceSynthesisAnalysisSpecifications)
+
+ParallelLogger::saveSettingsToJson(
+  esAnalysisSpecifications, 
+  file.path("inst/esAnalysisSpecification.json"))
+
+
+resultsExecutionSettings <- Strategus::createResultsExecutionSettings(
+  resultsDatabaseSchema = resultsDatabaseSchema,
+  resultsFolder = file.path("results", "evidence_sythesis", "strategusOutput"),
+  workFolder = file.path("results", "evidence_sythesis", "strategusWork")
+)
+
+Strategus::execute(
+  analysisSpecifications = esAnalysisSpecifications,
+  executionSettings = resultsExecutionSettings,
+  connectionDetails = resultsConnectionDetails
+)
+
+resultsDataModelSettings <- Strategus::createResultsDataModelSettings(
+  resultsDatabaseSchema = resultsDatabaseSchema,
+  resultsFolder = resultsExecutionSettings$resultsFolder,
+)
+
+Strategus::createResultDataModel(
+  analysisSpecifications = esAnalysisSpecifications,
+  resultsDataModelSettings = resultsDataModelSettings,
+  resultsConnectionDetails = resultsConnectionDetails
+)
+
+Strategus::uploadResults(
+  analysisSpecifications = esAnalysisSpecifications,
+  resultsDataModelSettings = resultsDataModelSettings,
+  resultsConnectionDetails = resultsConnectionDetails
+)
---
+++ UploadResultsFromZipFiles.R
@@ -0,0 +1,109 @@
+################################################################################
+# INSTRUCTIONS: The code below assumes you have used CreateResultsDataModel.R to
+# create the results data model in a Postgres or SQLite database. This script 
+# will loop over all of the directories found under the "results" folder and 
+# upload the results. 
+#
+# This script also contains some commented out code for 
+# setting read-only permissions for a user account on the results schema. 
+# This is used when setting up a read-only user for use with a Shiny results 
+# viewer. Additionally, there is commented out code that will allow you to run
+# ANALYZE on each results table to ensure the database is performant.
+# 
+# See the Working with results section
+# of the UsingThisTemplate.md for more details.
+# 
+# More information about working with results produced by running Strategus 
+# is found at:
+# https://ohdsi.github.io/Strategus/articles/WorkingWithResults.html
+# ##############################################################################
+
+# Settings ---------------------------------------------------------------------
+# Use the connnection  details to connect to either the Postgres or SQLite database:
+resultsDatabaseConnectionDetails <- DatabaseConnector::createConnectionDetails(
+  dbms = "sqlite",
+  server = "E:/exampleStrategusStudy/Results.sqlite"
+)
+resultsDatabaseSchema <- "main"
+resultsFiles <- list.files(path = "E:/shared_with_me/ExampleStudy", pattern = ".zip", full.names = T, recursive = F)
+
+
+# Don't make changes below this line -------------------------------------------
+analysisSpecifications <- ParallelLogger::loadSettingsFromJson(
+  fileName = "inst/studyAnalysisSpecification.json"
+)
+
+# Setup logging ----------------------------------------------------------------
+ParallelLogger::clearLoggers()
+ParallelLogger::addDefaultFileLogger(
+  fileName = "upload-log.txt",
+  name = "RESULTS_FILE_LOGGER"
+)
+ParallelLogger::addDefaultErrorReportLogger(
+  fileName = "upload-errorReport.txt",
+  name = "RESULTS_ERROR_LOGGER"
+)
+
+# Upload Results ---------------------------------------------------------------
+for (resultsFile in resultsFiles) {
+  tempFolder <- tempfile("StrategusResults")
+  dir.create(tempFolder)
+  unzip(resultsFile, exdir = tempFolder)
+  resultsDataModelSettings <- Strategus::createResultsDataModelSettings(
+    resultsDatabaseSchema = resultsDatabaseSchema,
+    resultsFolder = tempFolder,
+  )
+  
+  Strategus::uploadResults(
+    analysisSpecifications = analysisSpecifications,
+    resultsDataModelSettings = resultsDataModelSettings,
+    resultsConnectionDetails = resultsDatabaseConnectionDetails
+  )
+}
+
+# Optional scripts to set permissions and to analyze tables ------------------
+# connection <- DatabaseConnector::connect(
+#   connectionDetails = resultsDatabaseConnectionDetails
+# )
+# 
+# # Grant read only permissions to all tables
+# sql <- "GRANT USAGE ON SCHEMA @schema TO @results_user;
+# GRANT SELECT ON ALL TABLES IN SCHEMA @schema TO @results_user; 
+# GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA @schema TO @results_user;"
+# 
+# message("Setting permissions for results schema")
+# sql <- SqlRender::render(
+#   sql = sql, 
+#   schema = resultsDatabaseSchema,
+#   results_user = 'shinyproxy'
+# )
+# DatabaseConnector::executeSql(
+#   connection = connection, 
+#   sql = sql,
+#   progressBar = FALSE,
+#   reportOverallTime = FALSE
+# )
+#   
+# # Analyze all tables in the results schema
+# message("Analyzing all tables in results schema")
+# sql <- "ANALYZE @schema.@table_name;"
+# tableList <- DatabaseConnector::getTableNames(
+#   connection = connection,
+#   databaseSchema = resultsDatabaseSchema
+# )
+# for (i in 1:length(tableList)) {
+#   DatabaseConnector::renderTranslateExecuteSql(
+#     connection = connection,
+#     sql = sql,
+#     schema = resultsDatabaseSchema,
+#     table_name = tableList[i],
+#     progressBar = FALSE,
+#     reportOverallTime = FALSE
+#   )
+# }
+# 
+# DatabaseConnector::disconnect(connection)
+
+# Unregister loggers -----------------------------------------------------------
+ParallelLogger::unregisterLogger("RESULTS_FILE_LOGGER")
+ParallelLogger::unregisterLogger("RESULTS_ERROR_LOGGER")
